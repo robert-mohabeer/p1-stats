a23c84b29340c8cab4bd40072091ce6917a7924e
mohabeer <mohabeer@umich.edu>
2020-09-01 00:25:27 -0400

    Initial commit.  Starter files with function stubs.

---
+++ stats.cpp
@@ -0,0 +1,54 @@
+#include "stats.h"
+#include <cassert>
+#include <vector>
+using namespace std;
+
+vector<vector<double> > summarize(vector<double> v) {
+	assert(false);
+	return { {} };  // avoid Visual Studio error "function must return a value"
+}
+
+int count(vector<double> v) {
+	assert(false);
+	return 0;  // avoid Visual Studio error "function must return a value"
+}
+
+double sum(vector<double> v) {
+	assert(false);
+	return 0;  // avoid Visual Studio error "function must return a value"
+}
+
+double mean(vector<double> v) {
+	assert(false);
+	return 0;  // avoid Visual Studio error "function must return a value"
+}
+
+double median(vector<double> v) {
+	assert(false);
+	return 0;  // avoid Visual Studio error "function must return a value"
+}
+
+double mode(vector<double> v) {
+	assert(false);
+	return 0;  // avoid Visual Studio error "function must return a value"
+}
+
+double min(vector<double> v) {
+	assert(false);
+	return 0;  // avoid Visual Studio error "function must return a value"
+}
+
+double max(vector<double> v) {
+	assert(false);
+	return 0;  // avoid Visual Studio error "function must return a value"
+}
+
+double stdev(vector<double> v) {
+	assert(false);
+	return 0;  // avoid Visual Studio error "function must return a value"
+}
+
+double percentile(vector<double> v, double p) {
+	assert(false);
+	return 0;  // avoid Visual Studio error "function must return a value"
+}
